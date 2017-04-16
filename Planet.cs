24d726e97f70979e3e70adf8bd307975c5ae34a8
Reza-Amani <reza.amani@gmail.com>
2017-04-16 12:56:07 +1200

    Planet class started

---
+++ Planet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NeverSpace
+{
+    class Planet
+    {
+        int x, y, mass, radius;
+        float speed_x,speed_y;
+        
+
+        public Planet(int _x, int _y, int _mass, int _radius, float _speed_x, float _speed_y)
+        {
+            x = _x; y = _y; mass = _mass; radius = _radius; speed_x = _speed_x; speed_y = _speed_y;
+        }
+
+        public draw()
+        {
+        }
+    }
+}
